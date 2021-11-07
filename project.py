50c44e0b090598fc4a814efbda2d28b4ea7b4b09
GalaxyTwo612 <87010167+GalaxyTwo612@users.noreply.github.com>
2021-11-07 13:33:48 +0630

    Add files via upload

---
+++ project.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobil
+
+model = tf.keras.models.load_model(r"Waste_Classification_CNN.h5")
+
+st.title("Waste Classification CNN")
+file_upload = st.file_uploader("Upload an image")
+
+
+
+if file_upload is not None:
+    st.image(file_upload)
+    img = Image.open(file_upload)
+    img = img.resize((224,224))
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    img = img/255
+    img = np.expand_dims(img,axis=0)
+    result = model.predict(img)
+    btn=st.button("Check")
+    if btn:
+        if (result[0][1]>0.5):
+            obj="Recycle"
+            st.write(f'This object is {obj} object')
+        else:
+            obj='Organic'
+            st.write(f'This object is {obj} object')
+
+
+
